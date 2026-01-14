9da534e46f12a6d97c0501b2855ca39798888aa5
Glucoseo <maqsotabdulaziz@gmail.com>
2026-01-15 00:09:30 +0500

    text changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        About me, about this project
+        About me
       </h1>
       <p className="mb-4">
-        {`I'm an 14 y.o. NIS pupil from Uralsk, Kazakhstan. This is one of my training projects where i gain experience in web development.
+        {`I'm an 14 y.o. NIS pupil from Uralsk, Kazakhstan.
         I dream to build my own startup-unicorn, get accepted into one of the top universities in the world and become a great mentor.
-        Currently i'm making projects for my CVs, portfolio, Github, to gain experience in Next.js.(I NEED TO CHANGE BLOG INTO "MY THOUGHTS")
-        I appreciate any tip in my learning path, so any advice would be great.
-        I hope that in the future i would inspire and teach someone.`}
+        `}
       </p>
       <div className="my-8">
-{/*  <BlogPosts /> */}        
+{/*  <BlogPosts /> */}      
       </div>
     </section>
   )
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,7 +2,29 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Currently im working on a web-project called "Mully". Mully is an AI-powered academical guide for students where they can learn more about their topics of interests, make lectures and podcasts, preapare for their exams and more.</h1>
+    
+    <div className="flex flex-col gap-6 font-medium">
+      <div>
+        <div className="mb-8 text-2xl font-semibold tracking-tighter flex items-center justify-between">
+          <h1 className="font-semibold text-2xl mb-8 tracking-tighter">mully</h1><a className="underline underline-offset-2">2025 dec-now</a>
+        </div>
+        <p className="mb-4 text-xl">Mully is an AI-powered academical guide for students where they can learn more about their topics of interests, make lectures and podcasts, preapare for their exams and more.</p>
+      </div>
+      
+      <div>
+        <div className="mb-8 text-2xl font-semibold tracking-tighter flex items-center justify-between">
+          <h1 className="font-semibold text-2xl mb-8 tracking-tighter">mullyup</h1><a className="underline underline-offset-2">idk</a>
+        </div>
+        <a className="mb-4 text-xl">tbc</a>
+      </div>
+      
+      <div>
+        <div className="mb-8 text-2xl font-semibold tracking-tighter flex items-center justify-between">
+          <h1 className="font-semibold text-2xl mb-8 tracking-tighter">visucrypt</h1><a className="underline underline-offset-2">2026 jan-now</a>
+        </div>
+        <a className="mb-4 text-xl">tbc</a>
+      </div>
+    </div>
     </section>
   )
 }
